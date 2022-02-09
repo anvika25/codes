10ca217fb384bb8b1b6c8b31ffd9e7fcea8b4e4c
anvika <anvika@anvikas-MacBook-Pro.local>
2022-02-08 22:09:42 -0500

    added file reading

--- pythonemail/anvika.py
+++ pythonemail/anvika.py
@@ -1,15 +1,32 @@
+import time 
 import smtplib, ssl
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
 sender_email = "sumedhafromiim@gmail.com"  # Enter your address
-receiver_email = "mailtoanvika@gmail.com"  # Enter receiver address
 password = input("Enter password - ")
 message = """\
 Subject: Hi there
 
 This message is sent from Python."""
 
-with smtplib.SMTP_SSL(smtp_server, port) as server:
+with smtplib.SMTP_SSL(smtp_server, port) as server:  #to avoid try catch dinally for cloding of files 
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    with open('emails.csv', 'r') as file:
+        count = 0
+        
+        while True:
+            count += 1
+        
+            # Get next line from file
+            line = file.readline()
+        
+            # if line is empty
+            # end of file is reached
+            if not line:
+                break
+            print("Line{}: {}".format(count, line.strip()))
+            server.sendmail(sender_email,line.strip(), message)  
+            time.sleep(5)
+
+
